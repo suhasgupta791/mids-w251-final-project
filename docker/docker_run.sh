c7f68ce1b9b55b3654f4e37f9eab7c487bd5f450
Jonathan Zhou <zhou059@gmail.com>
2020-03-26 21:25:30 -0700

    client added

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -2,7 +2,7 @@
 
 docker run -d \
 	--runtime nvidia \
-	-v /mnt/sdb1/w251/mids-w251-final-project:/root \
+	-v /home/jzhou/project/mids-w251-final-project:/root \
 	-v /tmp:/tmp \
 	-p 8888:8888 \
 	-p 8000:8000 \
