c366d9980ba2b0af169e59a6818ab727f910d9b2
Suhas Gupta <gupta.suhas@gmail.com>
2020-03-29 08:59:44 -0700

    Training update on GPU

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -4,8 +4,8 @@ docker run -d \
 	--runtime nvidia \
 	-v $(pwd):/root \
 	-v /tmp:/tmp \
-	-p 8888:8888 \
-	-p 8000:8000 \
-	-p 6006:6006 \
+	-p 8887:8888 \
+	-p 8002:8000 \
+	-p 6007:6006 \
 	--name=w251-project \
 	pytorch_cuda
